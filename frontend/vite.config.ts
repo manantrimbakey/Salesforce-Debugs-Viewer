99a58ce1dfa686d61a20c01bf41adccf1910b927
manantrimbakey <manan.trimbakey@gmail.com>
2024-11-07 19:09:00 +0530

    .

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,35 +7,50 @@ import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": "Enter your backend host with port inside the quotes",
+    server: {
+        port: 3000,
+        proxy: {
+            // with options
+            "/setProjectPath": {
+                target: "http://localhost:4000/setProjectPath",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/setProjectPath/, ""),
+            },
+            "/getLogs": {
+                target: "http://localhost:4000/getLogs",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/getLogs/, ""),
+            },
+            "/getLogInfo": {
+                target: "http://localhost:4000/getLogInfo",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/getLogInfo/, ""),
+            },
+        },
     },
-  },
-  plugins: [
-    react(),
-    svgrPlugin({
-      svgrOptions: {
-        icon: true,
-      },
-    }),
-  ],
-  build: {
-    // outDir: "../build/frontend", // THIS SHOULD BE USED IN PRODUCTION
-    outDir: "./build",
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    css: true,
-    setupFiles: "./src/test/setup.ts",
-  },
+    plugins: [
+        react(),
+        svgrPlugin({
+            svgrOptions: {
+                icon: true,
+            },
+        }),
+    ],
+    build: {
+        // outDir: "../build/frontend", // THIS SHOULD BE USED IN PRODUCTION
+        outDir: "./build",
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`,
+            },
+        },
+    },
+    test: {
+        globals: true,
+        environment: "jsdom",
+        css: true,
+        setupFiles: "./src/test/setup.ts",
+    },
 });
